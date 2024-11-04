276e5f704dd5c77d68ea2aa58911fec07a117c78
Justice Philemon <imailasjay>
2024-11-04 16:15:34 +0100

    ...

--- src/index.mjs
+++ src/index.mjs
@@ -25,16 +25,10 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/users", (request, response) => {
-  response.send({ message: "Got all user!", users: mockUsers });
   const {
     query: { filter, value },
   } = request;
-
   if (!filter && !value) return response.send(mockUsers);
-  if (filter && value)
-    return response.send(
-      mockUsers.filter((user) => user[filter].includes(value))
-    );
 });
 
 app.get("/api/users/:id", (request, response) => {
