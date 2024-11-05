03b586aa765fc9cbbd6e4df7247172c2db37b16c
Justice Philemon <imailasjay>
2024-11-05 08:03:32 +0100

    PATCH request

--- src/index.mjs
+++ src/index.mjs
@@ -25,6 +25,7 @@ app.get("/", (request, response) => {
   response.send("Get request completed!");
 });
 
+// GET request
 app.get("/api/users", (request, response) => {
   const {
     query: { filter, value },
@@ -38,6 +39,7 @@ app.get("/api/users", (request, response) => {
   return response.send(mockUsers);
 });
 
+// POST request
 app.post("/api/users", (request, response) => {
   // unpacking in javascript: where { body } on the left side of the assignment means "take the body property from the request object and assign it to a new variable also named body"
   const { body } = request;
@@ -46,6 +48,7 @@ app.post("/api/users", (request, response) => {
   return response.status(200).send(newUser);
 });
 
+// GET request by identifier
 app.get("/api/users/:id", (request, response) => {
   const id = request.params.id;
   const parsedId = parseInt(id);
@@ -67,6 +70,20 @@ app.put("/api/users/:id", (request, response) => {
   const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
   if (findUserIndex === -1) return response.sendStatus(404);
   mockUsers[findUserIndex] = { id: parsedId, ...body };
+  return response.sendStatus(200);
+});
+
+// PATCH request: used for records where all of the fields may not be changes in a single request
+app.patch("/api/users/:id", (request, response) => {
+  const {
+    body,
+    params: { id },
+  } = request;
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return response.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return response.sendStatus(404);
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
   return response.sendStatus(200);
 });
 
