3fa9e2ea5d35cd1fdd40f624c9cf385ccf741869
Justice Philemon <imailasjay>
2024-11-05 08:53:33 +0100

    DELETE request

--- src/index.mjs
+++ src/index.mjs
@@ -87,6 +87,19 @@ app.patch("/api/users/:id", (request, response) => {
   return response.sendStatus(200);
 });
 
+// DELETE request
+app.delete("/api/users/:id", (request, response) => {
+  const {
+    params: { id },
+  } = request;
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return response.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return response.sendStatus(404);
+  mockUsers.splice(findUserIndex, 1);
+  return response.sendStatus(200);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
